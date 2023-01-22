a0808e50741568125187fed14bb95bee0ce6367f
Panagiotis Petridis <lpanagiotispetridisl@gmail.com>
2023-01-22 14:41:57 +0000

    minor change

--- cmd/cov-diff/main.go
+++ cmd/cov-diff/main.go
@@ -110,6 +110,7 @@ func main() {
 	}
 
 	fmt.Printf("Coverage on new lines: %d%%\n", percentCoverage)
+
 	if getActionInput("coverprofile") != "" {
 		outputErr := os.Setenv("GITHUB_OUTPUT", fmt.Sprintf(
 			"%s\n%s",
